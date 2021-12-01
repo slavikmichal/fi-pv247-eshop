32bf281bf2b813a149c33573aaa406a839884d43
Michal Slavík <xslavik@fi.muni.cz>
2021-12-01 18:44:47 +0100

    removed prototype products + get url lambda

--- uni-jas-eshop/src/utils/firebase.ts
+++ uni-jas-eshop/src/utils/firebase.ts
@@ -88,40 +88,5 @@ export const productsCollection = collection(
 export const productDocument = (id: string) =>
 	doc(db, 'product', id) as DocumentReference<Product>;
 
-// export const products: Product[] = [
-// 	{
-// 		id: 1,
-// 		name: 'Audi',
-// 		price: 100,
-// 		description: 'German car.',
-// 		image: 'audi.jpg'
-// 	},
-// 	{
-// 		id: 2,
-// 		name: 'BMW',
-// 		price: 80,
-// 		description: 'Another german car.',
-// 		image: 'bmw.jpeg'
-// 	},
-// 	{
-// 		id: 3,
-// 		name: 'Mercedes-Benz',
-// 		price: 120,
-// 		description: 'Yet another german car.',
-// 		image: 'mercedes.jpg'
-// 	},
-// 	{
-// 		id: 4,
-// 		name: 'Porsche',
-// 		price: 180,
-// 		description: 'German machine.',
-// 		image: 'porsche.jpg'
-// 	},
-// 	{
-// 		id: 5,
-// 		name: 'Nissan',
-// 		price: 200,
-// 		description: 'Sutututututu.',
-// 		image: 'nissan.jpg'
-// 	}
-// ];
+export const getUrl = async (id: string) =>
+	await getDownloadURL(ref(productsRef, `${id}.jpg`));
