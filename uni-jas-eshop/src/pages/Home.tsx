76949a3c15502a0b0d2d7b9e683ed339438c2055
Michal Slavík <xslavik@fi.muni.cz>
2021-12-04 18:52:06 +0100

    imgs refinement

--- uni-jas-eshop/src/pages/Home.tsx
+++ uni-jas-eshop/src/pages/Home.tsx
@@ -1,34 +1,41 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, useTheme } from '@mui/material';
 import { FC } from 'react';
 
 import HomeCard from '../components/HomeCard';
 
-const Home: FC = () => (
-	<>
-		<Typography variant="h4" textAlign="center">
-			Welcome on our website!
-		</Typography>
-		<Typography variant="h5" textAlign="center">
-			Uni-Jas is a place, where you can shop all the necessities for your
-			restaurant, pub or home.
-		</Typography>
-		<Grid container textAlign="center">
-			<Grid item md={2} />
-			<Grid item md={4} alignContent="center">
-				<HomeCard
-					router_link="/products"
-					imgSrc="/resources/products.png"
-					label="Enter eshop"
-				/>
+const Home: FC = () => {
+	const theme = useTheme();
+	return (
+		<>
+			<Typography variant="h4" textAlign="center">
+				Welcome on our website!
+			</Typography>
+			<Typography variant="h5" textAlign="center">
+				Uni-Jas is a place, where you can shop all the necessities for your
+				restaurant, pub or home.
+			</Typography>
+			<Grid container textAlign="center">
+				<Grid item md={2} />
+				<Grid item md={4} alignContent="center">
+					<HomeCard
+						router_link="/products"
+						imgSrc="/resources/products.png"
+						label="Enter eshop"
+					/>
+				</Grid>
+				<Grid item md={4} alignContent="center">
+					<HomeCard
+						router_link="/about"
+						imgSrc={
+							theme.palette.mode === 'light'
+								? '/resources/brand_light.svg'
+								: '/resources/brand_dark.svg'
+						}
+						label="About Us"
+					/>
+				</Grid>
 			</Grid>
-			<Grid item md={4} alignContent="center">
-				<HomeCard
-					router_link="/about"
-					imgSrc="/resources/brand_light.svg"
-					label="About Us"
-				/>
-			</Grid>
-		</Grid>
-	</>
-);
+		</>
+	);
+};
 export default Home;
